c19cc793874db4c3fd6d38eda8971f583c530dab
excavator <str_token@mail.ru>
2019-09-30 19:03:47 +0300

    err-pre

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -2,16 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-car',
-  template: `
-    <h2>Single Car</h2>
-    <p>Lorem ipsum  something yet</p>
-  `,
-  // styleUrls: ['./car.component.css']
-  styles: [`
-    h2 {
-      color: blue;
-    }
-    `]
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
 
