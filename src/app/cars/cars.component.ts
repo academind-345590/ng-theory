d1c787c75dd6606af1755f87f927616d4f5661fb
excavator <str_token@mail.ru>
2019-10-01 19:31:13 +0300

    tow way bindingy

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class CarsComponent {
   addCarStatus = '';
-  inputText = '';
+  inputText = 'Default text';
   constructor(){ }
   
   addCar(){
