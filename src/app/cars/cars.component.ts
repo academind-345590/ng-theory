b60a3ae6008316033568d9c1ee238975e4a976f1
excavator <str_token@mail.ru>
2019-10-01 21:17:46 +0300

    ngFor

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -7,11 +7,19 @@ import { Component } from '@angular/core';
 })
 export class CarsComponent {
   carName = '';
+  cars = ['Jiguli','Niva','Moskvich','UAZ'];
+  items = [
+    {id:2348, name: 'item 1.U.13'},
+    {id:9879, name: 'item 8.W.84'},
+    {id:2846, name: 'item 6.PP.56'},
+  ];
   addCarStatus = false;
   constructor(){ }
   
   addCar(){
     this.addCarStatus = true;
+    this.cars.push(this.carName);
+    this.carName = '';
   }
   
 }
