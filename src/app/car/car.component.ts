db54f419f2b13dde7f5bf970d6e179fa75fa706b
excavator <str_token@mail.ru>
2019-10-03 01:23:43 +0300

    component: extract html from ng-content

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ContentChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-car',
@@ -7,5 +7,9 @@ import { Component, Input } from '@angular/core';
 })
 export class CarComponent {
   @Input('carItem') car: {name: string, year: number};
- 
+  @ContentChild('carHeading', {static: false}) carHeading: ElementRef;
+
+  ngAfterViewInit(){
+    console.log(this.carHeading);
+  }
 }
