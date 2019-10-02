4c0990c8d7c5af6df8b58e4789cfcfbdc68fb25c
excavator <str_token@mail.ru>
2019-10-02 22:53:38 +0300

    component: viewport CSS

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,21 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
-  styleUrls: ['./cars.component.css']
+  styleUrls: ['./cars.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class CarsComponent {
  
   cars: {name: string, year: number}[] = [{
     name: 'Ford',
-    year: 2115
+    year: 2005
   },{
     name: 'Moskvich',
-    year: 2115
+    year: 2015
   },{
     name: 'BMW',
-    year: 2115
+    year: 2004
   }];
   
   constructor(){ }
