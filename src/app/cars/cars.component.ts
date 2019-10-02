911dbdb272b8cbb14beb86f35e6ee8d983867918
excavator <str_token@mail.ru>
2019-10-02 15:09:04 +0300

    pipe date , add and custom

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -9,6 +9,12 @@ export class CarsComponent {
   carName = '';
   cars = ['Jiguli','Niva','Moskvich','UAZ','Ford','BMW','Mazda'];
   addCarStatus = false;
+  dates = [
+    new Date(2000,3,17),
+    new Date(2015,1,21),
+    new Date(2012,8,28),
+    new Date(1997,12,2),
+  ]
   constructor(){ }
   
   addCar(){
@@ -16,8 +22,5 @@ export class CarsComponent {
     this.cars.push(this.carName);
     this.carName = '';
   }
-  countLengthName(car: string){
-    return car.length <= 3 ? true : false;
-  }
-  
+   
 }
