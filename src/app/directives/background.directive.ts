51ba020dbbb2c54225762a8f268ed6ab999f2509
excavator <str_token@mail.ru>
2019-10-03 20:00:35 +0300

    guidelines: event handling

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,16 +1,26 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener } from '@angular/core';
 
 @Directive ({
   selector: '[appBackground]',
 })
 
 export class BackgroundDirective implements OnInit {
+
+  @HostListener('mouseenter') mouseEnter(){
+    const {nativeElement} = this.element;
+    this.renderer.setStyle(nativeElement, 'background-color', 'red');
+    this.renderer.addClass(nativeElement, 'white-text')
+  }
+
+  @HostListener('mouseleave') mouseLeave(){
+    const {nativeElement} = this.element;
+    this.renderer.setStyle(nativeElement, 'background-color', 'transparent');
+    this.renderer.removeClass(nativeElement, 'white-text')
+  }
   
   constructor(private element: ElementRef, private renderer: Renderer2){}
 
   ngOnInit() {
-    const {nativeElement} = this.element;
-    this.renderer.setStyle(nativeElement, 'background-color', 'aqua');
-    this.renderer.addClass(nativeElement, 'white-text')
+    
   }
 }
