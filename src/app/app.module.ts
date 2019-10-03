9a6674e819e4d8353329961367d6e314ece65b5c
excavator <str_token@mail.ru>
2019-10-03 16:47:06 +0300

    guidelines: prepare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
-import { CarComponent } from './car/car.component';
-import { AddCarComponent } from './add-car/add-car.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent,
-    CarComponent,
-    AddCarComponent
   ],
   imports: [
     BrowserModule,
