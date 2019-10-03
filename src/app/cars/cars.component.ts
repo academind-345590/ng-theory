1f49915fd3901efa4917f10698f01e326cc4552b
excavator <str_token@mail.ru>
2019-10-03 16:16:44 +0300

    component: life cicle

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -10,18 +10,28 @@ export class CarsComponent {
   cars: {name: string, year: number}[] = [{
     name: 'Ford',
     year: 2005
-  },{
-    name: 'Moskvich',
-    year: 2015
-  },{
-    name: 'BMW',
-    year: 2004
-  }];
+  },
+  // {
+  //   name: 'Moskvich',
+  //   year: 2015
+  // },{
+  //   name: 'BMW',
+  //   year: 2004
+  // }
+  ];
   
   constructor(){ }
 
   updateCarList(car: {name: string, year: number}){
     this.cars.push(car);
+  }
+
+  changeCarName(){
+    this.cars[0].name = 'Drandulet'
+  }
+
+  deleteCar(){
+    this.cars.splice(0,1);
   }
   
 }
