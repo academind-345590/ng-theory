d356aac473af5ac62103f322e25a360696b941cb
excavator <str_token@mail.ru>
2019-10-03 18:29:10 +0300

    guidelines: create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BackgroundDirective } from './directives/background.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    BackgroundDirective,
   ],
   imports: [
     BrowserModule,
