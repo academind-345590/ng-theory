d4ec39a39603087adf5c040e98d563888392503b
excavator <str_token@mail.ru>
2019-10-03 21:59:54 +0300

    pipes: prepare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { BackgroundDirective } from './directives/background.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BackgroundDirective,
   ],
   imports: [
     BrowserModule,
