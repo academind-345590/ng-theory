b6d5b58b0e4cd67d64bb988ec226cf5ee6d80104
excavator <str_token@mail.ru>
2019-10-04 18:34:42 +0300

    pipes: overwiev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,34 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div class="col-md-8 col-md-offset-2">
-      
+      <h6>{{ name }}</h6>
+      <h6>{{ name | lowercase }}</h6>
+      <h6>{{ name | uppercase }}</h6>
+      <h6>{{ name | slice:3:6 | uppercase }}</h6>
+      <hr>
+      <h6>{{ pi }}</h6>
+      <h6>{{ pi | number }}</h6>
+      <h6>{{ pi | number: '1.3-5' }}</h6>
+      <h6>{{ pi | number: '2.2-2' }}</h6>
+      <hr>
+      <h6>{{ money | currency }}</h6>
+      <h6>{{ money | currency: 'TUGRIK' }}</h6>
+      <h6>{{ money | currency: 'EUR' : 'code' }}</h6>
+      <h6>{{ money | currency: 'EUR' : 'symbol' }}</h6>
+      <h6>{{ money | currency: 'EUR' : 'symbol-narrow' }}</h6>
+      <hr>
+      <h6>{{ date }}</h6>
+      <h6>{{ date | date }}</h6>
+      <h6>{{ date | date: 'longTime' }}</h6>
+      <h6>{{ date | date: 'shortDate' }}</h6>
+      <h6>{{ date | date: 'shortDate' }}</h6>
+      <h6>{{ date | date: 'shortDate' }}</h6>
+      <hr>
+      <h6>{{ amount | percent }}</h6>
+      <h6>{{ amount | percent: '4.3-5' }}</h6>
+      <hr>
+      <h6>{{ object }}</h6>
+      <pre>{{ object | json }}</pre>
     </div>
   `
 })
