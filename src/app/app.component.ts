de83fed385423ea559fd5b41c9c73cb6ea316157
excavator <str_token@mail.ru>
2019-10-05 16:17:43 +0300

    pipe: async pope

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Observable, interval } from 'rxjs';
+import { map, take } from 'rxjs/operators'; 
 
 @Component({
   selector: 'app-root',
   template: `
     <div class="col-md-8 col-md-offset-2">
+      <h1>{{  message$ | async }}</h1>
       <input type="text" class="form-control" [(ngModel)]="searchCar"><br>
       <button class="btn btn-primary" (click)="addCar()">Добавить машину</button>
       <hr>
@@ -31,4 +34,24 @@ export class AppComponent {
       name: "New Car", description: "New description"
     })
   }
+
+  message$: Observable<string>;
+
+  private messages = [
+    'One!',
+    'Two!',
+    'Three!',
+    'Go!'
+  ];
+
+  constructor() { this.resend(); }
+
+  resend() {
+    this.message$ = interval(1000).pipe(
+      map(i => this.messages[i]),
+      take(this.messages.length)
+    );
+  }
+
+
 }
