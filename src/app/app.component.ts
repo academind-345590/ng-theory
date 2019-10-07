28f6377258a69685744f9e0e0b21cf5b9ac4972e
excavator <str_token@mail.ru>
2019-10-07 16:16:30 +0300

    form: template-driven pattern : initial forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+
   answers = [{
     type: 'yes',
     text: 'Да'
@@ -13,4 +15,8 @@ export class AppComponent {
     type: 'no',
     text: 'Нет'
   }];
+
+  submitForm(form: NgForm){
+    console.log('Submited!', form);
+  }
 }
