877a848e97ca0ffffbe418194bcc3280e09bb448
excavator <str_token@mail.ru>
2019-10-07 12:58:52 +0300

    service: create

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { ConsoleService } from '../console.service';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
-  styleUrls: ['./car.component.css']
+  styleUrls: ['./car.component.css'],
+  providers: [ConsoleService]
 })
 export class CarComponent {
 
   @Input() car;
+
+  constructor(private consoleService: ConsoleService){};
 
   getClass() {
     return {
@@ -19,6 +23,7 @@ export class CarComponent {
 
   onAction(type: string) {
     this.car.isSold = type === 'buy' ? true : false;
+    this.consoleService.log(`${this.car.name} status = ${type}`);
   }
 
 }
