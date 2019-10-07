4b9c6c373350d196f93493584aa22db9415ffcb1
excavator <str_token@mail.ru>
2019-10-07 20:11:50 +0300

    form: template-driven pattern : validate 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,6 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [`
-    input.ng-invalid.ng-touched {
-      border: 1px solid red;
-    }
-  `]
 })
 export class AppComponent {
 
