dfd3d82dbe3f2593a47dd988a107749f32875303
excavator <str_token@mail.ru>
2019-10-08 15:59:25 +0300

    form: reactive: initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   answers = [{
     type: 'yes',
     text: 'Да'
@@ -13,4 +15,20 @@ export class AppComponent {
     type: 'no',
     text: 'Нет'
   }];
+
+  form: FormGroup
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      pass: new FormControl(''),
+      country: new FormControl('ru'),
+      answer: new FormControl('no')
+    });
+  }
+
+  onSubmit(){
+    console.log('Was Submited !', this.form);
+  }
+
 }
