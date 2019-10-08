ef10e4145adaeb9a17355e4881edb1e4f2b170de
excavator <str_token@mail.ru>
2019-10-08 18:30:54 +0300

    form: reactive: create async custom validator with promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { resolve, reject } from 'q';
 
 
 @Component({
@@ -22,7 +23,7 @@ export class AppComponent implements OnInit{
   ngOnInit() {
     this.form = new FormGroup({
       user: new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email]),
+        email: new FormControl('', [Validators.required, Validators.email],[this.checkForEmail]),
         pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)]),
       }),
       country: new FormControl('ru'),
@@ -41,6 +42,20 @@ export class AppComponent implements OnInit{
       return {lengthError: true}
     };
     return null;
+  }
+
+  checkForEmail(control: FormControl): Promise<any> {
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        if(control.value == 'test@test'){
+          resolve({
+            'emailIsUsed': true
+          });
+        } else {
+          resolve(null);
+        }
+      },3000)
+    })
   }
 
 }
