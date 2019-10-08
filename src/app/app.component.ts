717203bba29429ea0b39e4e58a7f6cc8deb6fa85
excavator <str_token@mail.ru>
2019-10-08 12:56:46 +0300

    form: reactive: prepare

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
 
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-
-  @ViewChild('form',{static: true}) form: NgForm;
-
   answers = [{
     type: 'yes',
     text: 'Да'
@@ -17,33 +13,4 @@ export class AppComponent {
     type: 'no',
     text: 'Нет'
   }];
-
-  defaultAnswer = "no";
-  defaultCantry = "by";
-
-  formData = {};
-  isSubmited = false;
-
-  submitForm(){
-    this.isSubmited = true;
-    //console.log('Submited!', this.form);
-    this.formData = this.form.value;
-    this.form.reset();    
-  }
-
-  clickRandEmail(){
-    const randEmail = 'rand@email.com';
-    // this.form.setValue({
-    //   user:{
-    //     pass: '',
-    //     email: randEmail
-    //   },
-    //   country: '',
-    //   answer: ''
-    // })
-    this.form.form.patchValue({
-      user: { email: randEmail }
-    });
-  }
-
 }
