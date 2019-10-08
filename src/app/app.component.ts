803db4fc7b5c9ed63801d50604233326a2486caf
excavator <str_token@mail.ru>
2019-10-08 18:45:44 +0300

    HTTP: prepare

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,61 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { resolve, reject } from 'q';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit{
-  
-  answers = [{
-    type: 'yes',
-    text: 'Да'
-  }, {
-    type: 'no',
-    text: 'Нет'
-  }];
-
-  form: FormGroup
-
-  ngOnInit() {
-    this.form = new FormGroup({
-      user: new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email],[this.checkForEmail]),
-        pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)]),
-      }),
-      country: new FormControl('ru'),
-      answer: new FormControl('no'),
-    });
-  }
-
-  onSubmit(){
-    console.log('Was Submited !', this.form);
-  }
-
-  charsCount = 6;
-
-  checkForLength(control: FormControl){
-    if(control.value.length < this.charsCount ){
-      return {lengthError: true}
-    };
-    return null;
-  }
-
-  checkForEmail(control: FormControl): Promise<any> {
-    return new Promise((resolve,reject)=>{
-      setTimeout(()=>{
-        if(control.value == 'test@test'){
-          resolve({
-            'emailIsUsed': true
-          });
-        } else {
-          resolve(null);
-        }
-      },3000)
-    })
-  }
-
+export class AppComponent{
+  cars = [
+    {
+      name: 'Ford',
+      color: 'white',
+      id: 1
+    }
+  ];
 }
