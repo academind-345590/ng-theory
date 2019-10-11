9809f0c66f076c708c805c0168acdadea68fe1f8
excavator <str_token@mail.ru>
2019-10-11 15:59:27 +0300

    Routers: create module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CarsService } from './cars.service';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CarsPageComponent } from './cars-page/cars-page.component';
-
-const appRoutes: Routes = [
-  { path: 'cars', component: CarsPageComponent },
-  { path: '', component: HomePageComponent }
-]
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,9 +19,7 @@ const appRoutes: Routes = [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(
-      appRoutes
-    )
+    AppRoutingModule    
   ],
   providers: [CarsService],
   bootstrap: [AppComponent]
