6f77382512dc1a373255cf0297df54fa52b7c064
excavator <str_token@mail.ru>
2019-10-14 17:52:04 +0300

    Animations: prepare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { HeaderComponent } from './header/header.component';
-import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
-
-
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule,
-    AppRoutingModule,
-    SharedModule    
+    FormsModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
