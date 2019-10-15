ed94d1ea1a7434a920bc0c0df8ba6733c5e135cc
excavator <str_token@mail.ru>
2019-10-15 14:21:20 +0300

    Animations: allocale animate to separete file

---
+++ src/app/app.animations.ts
@@ -0,0 +1,5 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+export const divTrigger = trigger('divTrigger',[
+
+]);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,71 +1,13 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { divTrigger } from './app.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   animations: [
-    trigger('clickedDiv', [
-      state('start', style({
-        backgroundColor: 'orange',
-        width: '150px',
-        height: '150px'
-      })),
-      state('end', style({
-        backgroundColor: 'green',
-        width: '300px',
-        height: '300px'
-      })),
-      state('active', style({
-        width: '170px',
-        height: '170px',
-        backgroundColor: 'pink'
-      })),
-      transition('start <=> end', [
-        animate('1500ms ease-in')
-      ]),
-      transition('stert => active',[
-        animate(400)
-      ]),
-      transition('active => end',[
-        animate(400)
-      ])
-    ]),
-    trigger('multi',[
-      state('start', style({
-        width: '150px',
-        height: '150px',
-        border: '4px solid black'
-      })),
-      state('end', style({
-        width: '170px',
-        height: '170px',
-        backgroundColor: 'green'
-      })),
-      transition('start <=> end', [
-        style({
-          backgroundColor: 'red'
-        }),
-        animate(1500, style({
-          backgroundColor: 'yellow'
-        })),
-        animate(1000, style({
-          width: '200px',
-          height: '200px'
-        })),
-        animate(1000)
-      ])
-    ])
+    divTrigger
   ]
 })
 export class AppComponent {
- clickedDivState ='start';
- multiState = 'start';
-
- changeDivState(){
-   this.clickedDivState = 'end';
-   setTimeout(()=>{
-     this.clickedDivState ='start';
-   },3000)
- }
+  isVisible = false;
 }
