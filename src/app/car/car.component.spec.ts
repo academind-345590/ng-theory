7ceb6f9e81bd588130720439391c76005f32b32e
excavator <str_token@mail.ru>
2019-10-16 18:18:58 +0300

    Test: service

--- src/app/car/car.component.spec.ts
+++ src/app/car/car.component.spec.ts
@@ -1,26 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { CarComponent } from './car.component';
+import { CarService } from './car.service';
 
 describe('CarComponent', () => {
+
+  let fixture: ComponentFixture<CarComponent>;
+  let component: CarComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [CarComponent]
-    })
+    });
+
+    fixture = TestBed.createComponent(CarComponent);
+    component = fixture.debugElement.componentInstance;
   })
 
-  it('should create component instance', () => {
-    const fixture = TestBed.createComponent(CarComponent);
-    const component = fixture.debugElement.componentInstance;
+  it('should create component instance', () => {    
     expect(component).toBeTruthy();
   });
 
   it(`should render tag h4  with text 'My Car Header'`, () => {
-    const fixture = TestBed.createComponent(CarComponent);
     fixture.detectChanges();
-    const component: HTMLElement = fixture.nativeElement;
-    const text = component.querySelector('h1');
-    expect(text.textContent).toEqual('My Car Header');
-  })
+    const element: HTMLElement = fixture.nativeElement;
+    const text = element.querySelector('h1').textContent;
+    expect(text).toEqual('My Car Header');
+  });
+
+  it('should inject CarService', () => {
+    const carService = fixture.debugElement.injector.get(CarService);
+    fixture.detectChanges();
+    expect(component.isCarVisible).toEqual(carService.getVisibility())
+  });
 
 });
